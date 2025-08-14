a784bf7f8a43ec8ea159ec5a0e436031515c9a50
Halfdan Sand Christensen <halfdansc@proton.me>
2025-08-14 11:59:34 +0200

    repository interface

--- Repository/ICinemaRepository.cs
+++ Repository/ICinemaRepository.cs
@@ -1,6 +1,15 @@
+using Cinema.Entities;
+
 namespace Cinema.Repository;
 
 public interface ICinemaRepository
 {
-    
+    List<Movie> GetAllMovies();
+    List<Screening> GetAllScreenings();
+    List<Screening> GetScreeningsByMovie(int movieId);
+    Hall GetHallForScreening(int screeningId);
+    bool BookSeat(int screeningId, int seatId, string guestName = null, string guestEmail = null, int? userId = null);
+    bool BlockSeat(int screeningId, int seatId, int minutesToBlock = 5);
+    int? GetSeatId(int hallId, int rowNumber, int seatNumber);
+    void CleanupExpiredBlocks();
 }
